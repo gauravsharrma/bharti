8ad7c663b817de5d9674de94272260f1ea26d428
gauravsharrma <gauravsharrma00@gmail.com>
2025-04-22 18:23:59 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+const components = [
+  "header", "hero", "about", "featured",
+  "why", "testimonials", "map", "blog", "contact", "footer"
+];
 
+components.forEach(async (id) => {
+  const res = await fetch(`components/${id}.html`);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+});
